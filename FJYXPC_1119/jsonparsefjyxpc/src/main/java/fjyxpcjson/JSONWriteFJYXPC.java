c99bf1d1c4708825eda89732114e78d910f6b869
Vlenard <58088389+Vlenard@users.noreply.github.com>
2025-11-19 11:27:15 +0100

    1119

---
+++ FJYXPC_1119/jsonparsefjyxpc/src/main/java/fjyxpcjson/JSONWriteFJYXPC.java
@@ -0,0 +1,52 @@
+package fjyxpcjson;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JSONWriteFJYXPC {
+    public static void main(String[] args) {
+        try {
+            InputStream is = JSONWriteFJYXPC.class.getResourceAsStream("/orarendFJYXPC.json");
+            InputStreamReader isr = new InputStreamReader(is);
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject)parser.parse(isr);
+
+            JSONObject root = (JSONObject) jsonObject.get("FJYXPC_orarend");
+            JSONArray lessons = (JSONArray) root.get("ora");
+
+            String buffer = "";
+            for (int i = 0; i < lessons.size(); i++) {
+                JSONObject lesson = (JSONObject) lessons.get(i);
+
+                buffer += "-------------------------\n";
+
+                buffer += "id: " + lesson.get("id") + "\n";
+                buffer += "Típus: " + lesson.get("tipus") + "\n";
+                buffer += "Tárgy: " + lesson.get("targy") + "\n";
+                buffer += "helyszyn: " + lesson.get("helyszin") + "\n";
+                buffer += "oktató: " + lesson.get("oktato") + "\n";
+                buffer += "szak: " + lesson.get("szak") + "\n";
+
+
+                JSONObject date = (JSONObject) lesson.get("idopont");
+                buffer += "Időpont:\n";
+                buffer += "\tNap: " + date.get("nap") + "\n";
+                buffer += "\ttol: " + date.get("tol") + "\n";
+                buffer += "\tig: " + date.get("ig") + "\n";
+
+                buffer += "------------------------\n";
+            }
+
+            System.out.println(buffer);
+
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
